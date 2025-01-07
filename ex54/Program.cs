0c93d78f63892e6179093b5c20c40cb163537cfc
Artem-0 <artiem.ruchii.99@mail.ru>
2025-01-07 14:14:41 +0300

    Выполнил задачу 54

---
+++ ex54/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 54. Задайте двумерный массив. Напишите программу, которая
+// упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] GenerateMatrix(int row, int col)
+{
+    Random rand = new Random();
+    int[,] matrix = new int[row, col];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            matrix[i, j] = rand.Next(10);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] SortRowsDescending(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+        {
+            for (int j = 0; j + 1 < matrix.GetLength(1); j++)
+            {
+                int temp;
+                if (matrix[i, j + 1] > matrix[i, j])
+                {
+                    temp = matrix[i, j + 1];
+                    matrix[i, j + 1] = matrix[i, j];
+                    matrix[i, j] = temp;
+                }
+            }
+        }
+    }
+    return matrix;
+}
+
+// --------------------------------------------------------
+
+int[,] matrix = GenerateMatrix(3, 10);
+System.Console.WriteLine("Исходная матрица:");
+PrintMatrix(matrix);
+
+System.Console.WriteLine("Сортированная матрица:");
+PrintMatrix(SortRowsDescending(matrix));
